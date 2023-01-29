a75f6183cbcc02694995cce2ac4eb316acccb864
Stefan Talbot <satalbot@protonmail.com>
2023-01-28 21:48:50 -0500

    Refactor Header Component HTML

--- to-shovel-or-not-react/src/components/Header.jsx
+++ to-shovel-or-not-react/src/components/Header.jsx
@@ -5,18 +5,20 @@ export default function Header() {
 
   return (
     <div className="bg-sky-400 h-32 flex flex-row">
-      <div className="basis-1/4 flex items-center">
+      <span className="basis-1/4 flex items-center">
         <h1 className="text-white font-logo text-7xl pl-5 drop-shadow-md">SaltyShovel</h1>
-      </div>
-      <span className="basis-3/4 flex flex-col items-center justify-center">
-        <p className="text-white font-sans font-bold text-lg">Where are you located?</p>
-        <input 
-          className="h-8 w-96 rounded-full text-center border-b-4" 
-          placeholder="Postal Code" 
-          onChange={e => setLocationInput(e.target.value)} 
-          value={locationInput}>
-        </input>
       </span>
+      <div className="basis-3/4 flex flex-col items-center justify-center">
+        <p className="text-white font-sans font-bold text-lg">Where are you located?</p>
+        <form>
+          <input 
+            className="h-8 w-96 rounded-full text-center border-b-4" 
+            placeholder="Postal Code" 
+            onChange={e => setLocationInput(e.target.value)} 
+            value={locationInput}>
+          </input>
+        </form>
+      </div>
       <span className="basis-1/4"/>
     </div>
   )
