80397320af6aac3a21fff28cbe69f305c8e73c40
Stefan Talbot <satalbot@protonmail.com>
2023-01-29 11:25:50 -0500

    Use .env Variables in Axios Call
    Refactor: Change url to use .env variables.
    Refactor: Remove apiCredentials.js import.

--- to-shovel-or-not-react/src/utils/axiosCalls.js
+++ to-shovel-or-not-react/src/utils/axiosCalls.js
@@ -1,14 +1,11 @@
 import axios from "axios";
-//couldn't get .env working so importing credentials will have to do for now
-import { APP_ID, APP_KEY } from './apiCredentials'
-
 //might be easier to use lat/long over postal code. request user location like many other sites
 
 //hardcoded location example: Toronto
 const hardCodedLocation = '43.65,-79.38'
 
 export const sevenDayForecast = (location) => {
-  return axios.get(`http://api.weatherunlocked.com/api/forecast/${hardCodedLocation}?app_id=${APP_ID}&app_key=${APP_KEY}`, {
+  return axios.get(`http://api.weatherunlocked.com/api/forecast/${hardCodedLocation}?app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_API_KEY}`, {
     headers: {
       'Accept': 'application/json',
     }
