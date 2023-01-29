9bd58355b24e4bfcc3f7dc11a90404b6b8ad0752
Stefan Talbot <satalbot@protonmail.com>
2023-01-28 21:26:12 -0500

    Import Font Families
    Feat: Add Inter font family.
    Feat: Add Modak font family.

--- to-shovel-or-not-react/tailwind.config.js
+++ to-shovel-or-not-react/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/components/**/*.jsx"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      logo: ['Modak', 'sans-serif']
+    }
   },
   plugins: [],
 }
