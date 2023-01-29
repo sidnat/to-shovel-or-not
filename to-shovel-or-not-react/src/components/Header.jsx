fc632a262ae5d0347cf4fc72f049c3e5fa806aa1
Stefan Talbot <satalbot@protonmail.com>
2023-01-28 19:54:58 -0500

    Add Placeholder HTML to Header Component
    Feat: Add placeholder HTML to Header Component to test Tailwindcss functionality.

--- to-shovel-or-not-react/src/components/Header.jsx
+++ to-shovel-or-not-react/src/components/Header.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
     </div>
   )
 }
