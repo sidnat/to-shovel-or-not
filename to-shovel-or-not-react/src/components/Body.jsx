720c364ba10c4a0e20275c514418837704aa1ea3
Stefan Talbot <satalbot@protonmail.com>
2023-01-29 20:00:50 -0500

    Implement Props and Conditional Rendering
    Feat: Added conditional rendering for WeatherCard components.
    Feat: Added props to WeatherCard components.
    Feat: WeatherCard components now display loaded data.

--- to-shovel-or-not-react/src/components/Body.jsx
+++ to-shovel-or-not-react/src/components/Body.jsx
@@ -3,25 +3,29 @@ import { useState, useEffect } from "react";
 import { sevenDayForecast } from "../utils/axiosCalls";
 import WeatherCard from "./WeatherCard";
 
-const Body = () => {
+export default function Body() {
   const [weatherData, setWeatherData] = useState(null)
 
   useEffect(() => {
     sevenDayForecast()
-      .then(forecast => setWeatherData(forecast))
+      .then(forecast => {
+        setWeatherData(forecast)
+      })
       .catch(err => console.log(err))
   }, [])
 
   return (
     <div className="flex flex-col h-full bg-sky-800">
       <div className="flex flex-row place-content-around h-full px-96">
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
+        {weatherData && 
+        <>
+          <WeatherCard date={'Today'} high={weatherData[0].high} low={weatherData[0].low} total_snow={weatherData[0].total_snow} total_rain={weatherData[0].total_rain}/>
+          <WeatherCard date={weatherData[1].date} high={weatherData[1].high} low={weatherData[1].low} total_snow={weatherData[1].total_snow} total_rain={weatherData[1].total_rain}/>
+          <WeatherCard date={weatherData[2].date} high={weatherData[2].high} low={weatherData[2].low} total_snow={weatherData[2].total_snow} total_rain={weatherData[2].total_rain}/>
+        </>
+        }
       </div>
       {/* Determination Component Goes Here */}
     </div>
   )
 }
-
-export default Body;
--- to-shovel-or-not-react/src/components/WeatherCard.jsx
+++ to-shovel-or-not-react/src/components/WeatherCard.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { BsCloudSnow, BsSnow3, BsDroplet } from 'react-icons/bs'
 
 export default function WeatherCard(props) {
-  const {date, high, low, snow, rain, timeframes} = props;
+  const {date, high, low, total_snow, total_rain, timeframes} = props;
+
+  console.log(high, low, total_rain, total_snow);
+
   return (
     <div className="flex flex-col mt-6 w-1/6 h-5/6 p-5 text-white bg-sky-400 font-bold drop-shadow-md rounded-lg">
       <div className="flex flex-row place-content-between items-end py-3">
         <BsCloudSnow className="h-full w-1/3 py-3"/>
-        <p className="text-3xl">Monday</p>
+        <p className="text-3xl">{date}</p>
       </div>
       <div className="bg-pink-500 w-full h-2/5 text-center">
         CHART AREA
@@ -15,21 +18,21 @@ export default function WeatherCard(props) {
       <div className="flex flex-row h-1/6 place-content-around pt-3 text-center text-2xl">
         <span>
           <p>HIGH</p>
-          <p className="text-4xl">8{'\u00b0'}</p>
+          <p className="text-4xl">{`${high}\u00b0`}</p>
         </span>
         <span>
           <p>LOW</p>
-          <p className="text-4xl">-8{'\u00b0'}</p>
+          <p className="text-4xl">{`${low}\u00b0`}</p>
         </span>
       </div>
       <div className="flex flex-row h-1/6 place-content-around pt-3 text-center">
         <span className="flex flex-col items-center">
           <BsDroplet className="h-10 w-10"/>
-          <p className="text-xl">8mm</p>
+          <p className="text-xl">{`${total_rain}mm`}</p>
         </span>
         <span className="flex flex-col items-center">
           <BsSnow3 className="h-10 w-10"/>
-          <p className="text-xl">60mm</p>
+          <p className="text-xl">{`${total_snow}mm`}</p>
         </span>
       </div>
     </div>
