a7b807dd3d3e6e9d0d0a19372cf9a185e64b1a8f
Stefan Talbot <satalbot@protonmail.com>
2023-01-30 17:02:33 -0500

    Add Xaxis to Chart Component

--- to-shovel-or-not-react/src/components/Chart.jsx
+++ to-shovel-or-not-react/src/components/Chart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AreaChart, Area, YAxis, ResponsiveContainer, Legend } from 'recharts';
+import { AreaChart, Area, YAxis, ResponsiveContainer, Legend, XAxis } from 'recharts';
 
 export default function Chart(props) {
   const { data } = props;
@@ -15,6 +15,7 @@ export default function Chart(props) {
           }}>
           <Legend verticalAlign="top" height={32} align={"right"} iconType={"circle"}/>
           <YAxis />
+          <XAxis tick={false}/>
           <Area name="Snow Accum mm" type="monotone" dataKey="snow_accum_cm" stackId="1" stroke="#FFFFFF" fill="#FFFFFF" />
           <Area name="Rain mm" type="monotone" dataKey="rain_mm" stackId="1" stroke="#075985" fill="#075985" />
         </AreaChart>
