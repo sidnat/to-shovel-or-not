ab8f47dbaa0e2fc341e16ed2c50c14ac318a3918
Stefan Talbot <satalbot@protonmail.com>
2023-01-30 19:38:15 -0500

    Implement Geocoder

--- to-shovel-or-not-react/src/components/Header.jsx
+++ to-shovel-or-not-react/src/components/Header.jsx
@@ -1,8 +1,21 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { sevenDayForecast } from "../utils/axiosCalls";
 
-export default function Header() {
-  // TODO: Move this state to the parent component.
+export default function Header(props) {
+  const { onSubmit } = props;
   const [locationInput, setLocationInput] = useState('')
+
+  // TODO: I don't think the axios request should live here, perhaps refactor later?
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`http://api.positionstack.com/v1/forward?access_key=${process.env.REACT_APP_POSITION_STACK_KEY}&query=${locationInput}`)
+      .then((res) => {
+        const coordinates = [res.data.data[0].latitude, res.data.data[0].longitude].join(',')
+        onSubmit(coordinates)
+      })
+      .catch(err => {console.log(Error(err))})
+  };
 
   return (
     <div className="bg-sky-400 h-40 flex flex-row">
@@ -11,7 +24,7 @@ export default function Header() {
       </span>
       <div className="basis-3/4 flex flex-col items-center justify-center">
         <p className="text-white font-sans font-bold text-lg">Where are you located?</p>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <input 
             className="h-8 w-96 rounded-full text-center border-b-4" 
             placeholder="Postal Code" 
