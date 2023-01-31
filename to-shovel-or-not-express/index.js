6cdd5a13eec53dbb7feaa1b4003b17fc1d96f934
Sid Natarajan <sid.nat@gmail.com>
2023-01-31 07:15:06 -0500

    implemented express route for location data

--- to-shovel-or-not-express/index.js
+++ to-shovel-or-not-express/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3003
 const { threeDayForecast } = require('./utils/weather-api-axios')
+const { getLongLat } = require('./utils/location-api-axios')
 var cors = require('cors')
 app.use(cors())
 
@@ -13,6 +14,11 @@ var corsOptions = {
 app.get("/getForecast", [cors(corsOptions)], async (req, res) => {
   const forecast = await threeDayForecast(req.query.location)
   res.send(forecast)
+})
+
+app.get("/getLongLat", [cors(corsOptions)], async (req, res) => {
+  const longLat = await getLongLat(req.query.locationInput)
+  res.send(longLat)
 })
 
 app.listen(port, () => {
