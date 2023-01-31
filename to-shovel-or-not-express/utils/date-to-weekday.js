a2bed7fab5027bdd492a6b2d2b128091d78748d0
Sid Natarajan <sid.nat@gmail.com>
2023-01-31 02:46:25 -0500

    brought helper functions over from react directory

---
+++ to-shovel-or-not-express/utils/date-to-weekday.js
@@ -0,0 +1,8 @@
+const convertDateToWeekday = (dateStr) => {
+  const splitDate = dateStr.split('/')
+  const day = new Date(`${splitDate[1]}/${splitDate[0]}/${splitDate[2]}`).getDay()
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  return days[day]
+}
+
+module.exports = { convertDateToWeekday }  
---
+++ to-shovel-or-not-express/utils/weather-api-axios.js
@@ -0,0 +1,63 @@
+const axios = require('axios')
+const { convertDateToWeekday } = require('./date-to-weekday')
+require('dotenv').config()
+
+//hardcoded location example: Toronto
+const defaultLocation = '43.65,-79.38'
+
+// will be moved to express backend in order to hide API key and ID
+const sevenDayForecast = (location) => {
+  location = location ? location : defaultLocation
+
+  return axios.get(`http://api.weatherunlocked.com/api/forecast/${location}?app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_API_KEY}`, {
+    headers: {
+      'Accept': 'application/json',
+    }
+  })
+    .then((forecastObj) => {
+      const weekForecast = []
+
+      if (forecastObj.data.Days) {
+        for (let day of forecastObj.data.Days) {
+          const eachDay = {
+            date: convertDateToWeekday(day.date),
+            high: day.temp_max_c,
+            low: day.temp_min_c,
+            total_snow: day.snow_total_mm,
+            total_rain: day.rain_total_mm,
+            Timeframes: []
+          }
+
+          if (day.Timeframes) {
+            for (let timeSegment of day.Timeframes) {
+              const snow_accum_mm = timeSegment.snow_accum_cm * 10;
+
+              const eachTimeframe = {
+                time: timeSegment.time,
+                wx_code: timeSegment.wx_code,
+                weather_desc: timeSegment.wx_desc,
+                weather_icon: timeSegment.wx_icon,
+                temp_c: timeSegment.temp_c,
+                feelslike_c: timeSegment.feelslike_c,
+                rain_mm: timeSegment.rain_mm,
+                snow_mm: timeSegment.snow_mm,
+                snow_accum_mm: snow_accum_mm
+              }
+
+              eachDay.Timeframes.push(eachTimeframe)
+            }
+          }
+
+          weekForecast.push(eachDay)
+        }
+
+        // console.log(weekForecast)
+
+        return weekForecast
+      }
+
+      return null
+    })
+}
+
+module.exports = { sevenDayForecast }
