9b265e5bf9e3c9b3e4f89b3e33f470281c68acd2
Sid Natarajan <sid.nat@gmail.com>
2023-01-31 07:20:42 -0500

    implemented react-cookie for location coordinates and location label

--- to-shovel-or-not-react/src/components/Index.jsx
+++ to-shovel-or-not-react/src/components/Index.jsx
@@ -3,27 +3,27 @@ import Body from "./Body";
 import Footer from "./Footer";
 import Header from "./Header";
 import { threeDayForecast } from "../utils/axiosCalls";
+import { useCookies } from 'react-cookie';
 
 export default function Index() {
   const [weatherData, setWeatherData] = useState(null)
+  const [cookies, setCookie] = useCookies(['location', 'label']);
 
-  const onSubmit = (location) => {
-    console.log('submit location:', location);
-    threeDayForecast(location)
-      .then(forecast => setWeatherData(forecast))
-      .catch(err => console.log(Error(err)))
+  const onSubmit = (location, label) => {
+    setCookie('location', location, { path: '/' })
+    setCookie('label', label, { path: '/' })
   }
 
   useEffect(() => {
-    threeDayForecast()
+    threeDayForecast(cookies.location)
       .then(forecast => setWeatherData(forecast))
       .catch(err => console.log(Error(err)))
-  }, [])
+  }, [cookies.location])
 
 
   return (
     <div className='flex flex-col' style={{height: '100vh'}}>
-      <Header onSubmit={onSubmit}/>
+      <Header onSubmit={onSubmit} label={cookies.label}/>
       <Body weatherData={weatherData}/>
       <Footer />
     </div>
