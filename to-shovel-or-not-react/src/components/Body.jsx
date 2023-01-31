17d7558dc450f96178bb62f0f2380b9569e5b693
Sid Natarajan <sid.nat@gmail.com>
2023-01-31 02:54:58 -0500

    passed down determination to body

--- to-shovel-or-not-react/src/components/Body.jsx
+++ to-shovel-or-not-react/src/components/Body.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import WeatherCard from "./WeatherCard";
+import Determination from "./Determination";
 
 export default function Body(props) {
   const { weatherData } = props;
@@ -8,10 +9,22 @@ export default function Body(props) {
   function drawWeatherCards() {
     if (!weatherData) return null
     return weatherData.map((day, i) => {
-      return(
-        <WeatherCard key={i} date={(i === 0 ? 'Today' : day.date)} high={day.high} low={day.low} total_snow={day.total_snow} total_rain={day.total_rain} timeframes={day.Timeframes}/>
+      return (
+        <WeatherCard key={i} date={(i === 0 ? 'Today' : day.date)} high={day.high} low={day.low} total_snow={day.total_snow} total_rain={day.total_rain} timeframes={day.Timeframes} />
       );
     })
+  }
+
+  // Creates Determination components
+  const drawDeterminations = () => {
+    if (!weatherData) return null
+
+    return weatherData.map((day, i) => {
+      return (
+        <Determination key={i} high={day.high} low={day.low} total_snow={day.total_snow} total_rain={day.total_rain} />
+      )
+    })
+
   }
 
   return (
@@ -19,7 +32,9 @@ export default function Body(props) {
       <div className="flex flex-row place-content-around h-full px-96">
         {drawWeatherCards()}
       </div>
-      {/* Determination Component Goes Here */}
+      <div>
+        {drawDeterminations()}
+      </div>
     </div>
   )
 }
