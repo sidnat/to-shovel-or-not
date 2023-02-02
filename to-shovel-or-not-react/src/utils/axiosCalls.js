dce64b65131fd5c3cd34383b4582eb6f9474fd80
Sid Natarajan <sid.nat@gmail.com>
2023-02-02 17:55:31 -0500

    added vercel backend url to axios call

--- to-shovel-or-not-react/src/utils/axiosCalls.js
+++ to-shovel-or-not-react/src/utils/axiosCalls.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
+// should be in a fetches folder
+// file for each axios
+
 //hardcoded location example: Toronto
 const defaultLocation = '43.65,-79.38'
 
 export const threeDayForecast = (location) => {
   location = location ? location : defaultLocation
 
-  return axios.get(`http://localhost:3003/getForecast?location=${location}`)
+  return axios.get(`https://salty-shovel-express.vercel.app/getForecast?location=${location}`)
     .then((forecastObj) => {
       return forecastObj.data
     })
@@ -17,7 +20,7 @@ export const threeDayForecast = (location) => {
 }
 
 export const getLongLatAndLabel = (locationInput) => {
-  return axios.get(`http://localhost:3003/getLongLat?locationInput=${locationInput}`)
+  return axios.get(`https://salty-shovel-express.vercel.app/getLongLat?locationInput=${locationInput}`)
   .then((longLat) => {
     return longLat.data
   })
