334ad5ad3905069741fbaa968d52b327019382e1
Sid Natarajan <sid.nat@gmail.com>
2023-02-02 17:54:25 -0500

    fixed determination card width, formatted code

--- to-shovel-or-not-react/src/components/Determination.jsx
+++ to-shovel-or-not-react/src/components/Determination.jsx
@@ -7,11 +7,11 @@ export default function Determination(props) {
   const { status } = props;
 
   return (
-    <div className="flex flex-col items-center place-content-between bg-sky-400 rounded-md h-48 w-96 p-4 drop-shadow-md mx-4">
+    <div className="flex flex-col items-center place-content-between bg-sky-400 rounded-md h-48 w-80 p-4 drop-shadow-md mx-4">
       <div className="text-center w-96">{selectRandMessage(status)}</div>
-      {status === 0 && <FaUmbrellaBeach className="h-24 w-24 rounded-full border-slate-800 border-4"/>}
-      {(status === 1 || status === 3) && <TbShovel className="h-24 w-24 rounded-full border-slate-800 border-4"/>}
-      {status === 2 && <TbSalt className="h-24 w-24 rounded-full border-slate-800 border-4"/>}
+      {status === 0 && <FaUmbrellaBeach className="h-24 w-24 rounded-full border-slate-800 border-4" />}
+      {(status === 1 || status === 3) && <TbShovel className="h-24 w-24 rounded-full border-slate-800 border-4" />}
+      {status === 2 && <TbSalt className="h-24 w-24 rounded-full border-slate-800 border-4" />}
     </div>
   )
 }
